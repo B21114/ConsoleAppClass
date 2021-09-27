d4bd43b92afc938395bdff75704d8826546f8841
B21114 <b21114@yandex.ru>
2021-09-27 17:54:24 +0400

    Задание 3, временный вариант

--- ConsoleAppClass1Zad3/Program.cs
+++ ConsoleAppClass1Zad3/Program.cs
@@ -11,52 +11,110 @@ namespace ConsoleAppClass1Zad3
 {
 
 
-    public class Book()
+    public class Book
+    {
+        public Title title;
+        public Author author;
+        public Content content;
+        public Book(string title, string author, string content)
         {
-          Book(string title, string author, string content)
-    { 
-    
-    }
+            this.title = new Title(title);
+            this.author = new Author(author);
+            this.content = new Content(content);
+           
         }
-public class Title
-{
-    public string title { get; set; }
-    void Show()
-    {
-        Console.ForegroundColor = ConsoleColor.Green; // устанавливаем цвет
-        Console.WriteLine("...");
-        Console.ResetColor();
-    }
-}
-class Author
-{
-    public string author { get; set; }
-    void Show()
-    {
-        Console.ForegroundColor = ConsoleColor.Red; // устанавливаем цвет
-        Console.WriteLine("...");
-        Console.ResetColor();
-    }
+        public Book()
+        {
+            this.title = new Title();
+            this.author = new Author();
+            this.content = new Content();
+        }
 
-}
-class Content
-{
-    public string content { get; set; }
-    void Show()
-    {
-        Console.ForegroundColor = ConsoleColor.Yellow; // устанавливаем цвет
-        Console.WriteLine("...");
-        Console.ResetColor();
-    }
-}
-class Program
-{
-    static void Main(string[] args)
-    {
-        //  Book book1 = new Book();
-        // Book1.
-    }
-}
+
 
     }
+    public class Title
+    {
+        public string title { get; set; }
+        public Title()
+        {
+
+        }
+        public Title(string title)
+        {
+            this.title = title;
+        }
+        public void Show()
+        {
+            Console.ForegroundColor = ConsoleColor.Green; // устанавливаем цвет
+            Console.WriteLine($"Название: {title}");
+            Console.ResetColor();
+        }
+    }
+    public class Author
+    {
+        public string author { get; set; }
+        public Author()
+        {
+
+        }
+        public Author(string author)
+        {
+            this.author = author;
+        }
+        public void Show()
+        {
+            Console.ForegroundColor = ConsoleColor.Red; // устанавливаем цвет
+            Console.WriteLine($"Автор: {author}");
+            Console.ResetColor();
+        }
+
+    }
+   public class Content
+    {
+        public string content { get; set; }
+        public Content()
+        {
+
+        }
+        public Content(string content)
+        {
+            this.content = content;
+        }
+       public void Show()
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow; // устанавливаем цвет
+            Console.WriteLine($"Описание: {content}");
+            Console.ResetColor();
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Book book = new Book()
+
+            book.title.title = "Война";
+            book.author.author = "Леха";
+            book.content.content = "Про войну";
+            book.title.Show();
+            book.author.Show();
+            book.content.Show();
+
+          
+            Book book1 = new Book();
+            Console.WriteLine("Введите автора");
+            book1.author.author = Console.ReadLine();
+            Console.WriteLine("Введите описание");
+            book1.content.content = Console.ReadLine();
+            Console.WriteLine("Введите название");
+            book1.title.title = Console.ReadLine();
+
+            book1.title.Show();
+            book1.author.Show();
+            book1.content.Show();
+
+        }
+    }
+
 }
