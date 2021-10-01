d47a84001067c25e93f8fcc08c04119b0c2e776c
B21114 <b21114@yandex.ru>
2021-10-01 17:09:44 +0400

    Выполнено задание 0 лист 3

---
+++ ConsoleApp3Zad0/Program.cs
@@ -0,0 +1,75 @@
+﻿/*
+Создайте класс Printer.
+В теле класса создайте метод void Print(string value), который выводит на экран значение
+аргумента.
+Реализуйте возможность того, чтобы в случае наследования от данного класса других классов, и вызове
+соответствующего метода их экземпляра, строки, переданные в качестве аргументов методов,
+выводились разными цветами.
+Обязательно используйте приведение типов. */
+using System;
+
+namespace ConsoleApp3Zad0
+{
+    /// <summary>
+    /// Класс принтер 
+    /// </summary>
+    public class Printer
+    {
+        /// <summary>
+        /// Метод вывода информации обычным цветом
+        /// </summary>
+        /// <param name="value">Строка</param>
+        public virtual void Print(string value)
+        {
+            Console.WriteLine(value);
+        }
+    }
+
+    /// <summary>
+    /// Класс красный, наследник принтера
+    /// </summary>
+    public class Red : Printer
+    {
+        /// <summary>
+        /// Метод перекраски в красный
+        /// </summary>
+        /// <param name="value">Строка</param>
+        public override void Print(string value)
+        {
+            Console.ForegroundColor = ConsoleColor.Red; // устанавливаем цвет
+            Console.WriteLine($"{value}");
+            Console.ResetColor(); // сбрасываем в стандартный
+        }
+    }
+
+    /// <summary>
+    /// Класс зеленый, наследник принтера
+    /// </summary>
+    public class Green : Printer
+    {
+        /// <summary>
+        /// Метод перекраски в зеленый
+        /// </summary>
+        /// <param name="value">Строка</param>
+        public override void Print(string value)
+        {
+            Console.ForegroundColor = ConsoleColor.Green; // устанавливаем цвет
+            Console.WriteLine($"{value}");
+            Console.ResetColor(); // сбрасываем в стандартный
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Printer printer = new Printer();
+            printer.Print("Строка1");
+            Red red = new Red();
+            red.Print("Строка2");
+            Green green = new Green();
+            green.Print("Строка3");
+        }
+    }
+}
---
+++ ConsoleApp3zad2/Program.cs
@@ -0,0 +1,44 @@
+﻿/*Создать класс, представляющий учебный класс ClassRoom.
+Создайте класс ученик Pupil. В теле класса создайте методы void Study(), void Read(), void
+Write(), void Relax().
+Создайте 3 производных класса ExcelentPupil, GoodPupil, BadPupil от класса базового класса
+Pupil и переопределите каждый из методов, в зависимости от успеваемости ученика.
+Конструктор класса ClassRoom принимает аргументы типа Pupil, класс должен состоять из 4 учеников.
+Предусмотрите возможность того, что пользователь может передать 2 или 3 аргумента.
+Выведите информацию о том, как все ученики экземпляра класса ClassRoom умеют учиться, читать,
+писать, отдыхать.*/
+
+using System;
+
+namespace ConsoleApp3zad2
+{
+    class Pupil
+    {
+        public string Name { get; set; }
+        public string Fname { get; set; }
+        public double Ocenki { get; set; }
+        public double Povedenie { get; set; }
+        public Pupil()
+        { }
+        void Study() {
+            Console.WriteLine("Учится"); 
+        }
+        void Read() {
+            Console.WriteLine("Читает");
+        }
+        void Write() {
+            Console.WriteLine("Пишет");
+        }
+        void Relax() {
+            Console.WriteLine("Отдыхает");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+         //   Pupil pupil = new Pupil("1","2",3,4);
+        
+        }
+    }
+}
