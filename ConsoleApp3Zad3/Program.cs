b507a660609d3a89b60a0dc462b14f23c4ea8b24
B21114 <b21114@yandex.ru>
2021-10-05 13:39:11 +0400

    Лист 3, задание 2 выполнено

---
+++ ConsoleApp3Zad3/Program.cs
@@ -0,0 +1,57 @@
+﻿/*
+ Создать класс Vehicle.
+В теле класса создайте поля: координаты и параметры средств передвижения (цена, скорость, год
+выпуска).
+Создайте 3 производных класса Plane, Саг и Ship.
+Для класса Plane должна быть определена высота и количество пассажиров.
+Для класса Ship — количество пассажиров и порт приписки.
+Написать программу, которая выводит на экран информацию о каждом средстве передвижения. 
+ */
+
+using System;
+
+namespace ConsoleApp3Zad3
+
+{
+
+    class Vehicle
+    {
+        double X { get; set; }
+        double Y { get; set; }
+        double Price { get; set; }
+        double Speed { get; set; }
+        double Year { get; set; }
+      
+    }   
+    class Plane : Vehicle
+    {
+        double Height { get; set; }
+        int Passenger { get; set; }
+     
+    }
+    
+    class Ship : Vehicle
+    {
+        string Port { get; set; }
+        int Passenger { get; set; }
+
+        public void Move()
+        {
+            Console.WriteLine("");
+        }
+    }
+    class Car : Vehicle
+    {
+        public void Move()
+        {
+            Console.WriteLine("Машина едет");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
--- ConsoleApp3zad2/Program.cs
+++ ConsoleApp3zad2/Program.cs
@@ -15,30 +15,132 @@ namespace ConsoleApp3zad2
     class Pupil
     {
         public string Name { get; set; }
-        public string Fname { get; set; }
-        public double Ocenki { get; set; }
-        public double Povedenie { get; set; }
         public Pupil()
-        { }
-        void Study() {
-            Console.WriteLine("Учится"); 
+        {
+
         }
-        void Read() {
-            Console.WriteLine("Читает");
+        public Pupil(string name)
+        {
+            Name = name;
         }
-        void Write() {
-            Console.WriteLine("Пишет");
+        public virtual void Study() { }
+        public virtual void Read() {}
+        public virtual void Write() {}
+        public virtual void Relax() {}
+
+        public void Info()
+        {
+            Console.WriteLine();
         }
-        void Relax() {
-            Console.WriteLine("Отдыхает");
+    }
+
+    class ExcelentPupil:Pupil
+    {
+        public ExcelentPupil(string name)
+        {
+            Name = name;
+        }
+
+        public override void Study()
+        {
+            Console.WriteLine("Лучшие люди учатся");
+        }
+        public override void Read()
+        {
+            Console.WriteLine("Лучшие люди пишут");
+        }
+        public override void Write()
+        {
+            Console.WriteLine("Лучшие люди читают");
+        }
+        public override void Relax()
+        {
+            Console.WriteLine("Лучшие люди отдыхают");
+        }
+    }
+    class GoodPupil : Pupil
+    {
+        public GoodPupil(string name)
+        {
+            Name = name;
+        }
+        public override void Study()
+        {
+            Console.WriteLine("Хорошие люди учатся");
+        }
+        public override void Read()
+        {
+            Console.WriteLine("Хорошие люди пишут");
+        }
+        public override void Write()
+        {
+            Console.WriteLine("Хорошие люди читают");
+        }
+        public override void Relax()
+        {
+            Console.WriteLine("Лучшие люди отдыхают");
+        }
+    }
+    class BadPupil : Pupil
+    {
+        public BadPupil(string name)
+        {
+            Name = name;
+        }
+        public override void Study()
+        {
+            Console.WriteLine("Плохие люди учатся");
+        }
+        public override void Read()
+        {
+            Console.WriteLine("Плохие люди пишут");
+        }
+        public override void Write()
+        {
+            Console.WriteLine("Плохие люди читают");
+        }
+        public override void Relax()
+        {
+            Console.WriteLine("Плохие люди отдыхают");
+        }
+    }
+    class ClassRoom
+    {
+        Random rnd = new Random();
+        Pupil[] pupils;
+
+        public ClassRoom(Pupil pupil1, Pupil pupil2)
+        {
+            pupils = new Pupil[] { pupil1, pupil2};
+                for (int i = 0; i < pupils.Length; i++)
+                {
+                   // pupils[i] = Convert.ToString(rnd.Next());
+
+                }
+            }
+        public ClassRoom(Pupil pupil1, Pupil pupil2, Pupil pupil3)
+        {
+            pupils = new Pupil[] { pupil1, pupil2, pupil3};
+        }
+        public ClassRoom(Pupil pupil1, Pupil pupil2, Pupil pupil3, Pupil pupil4)
+        {
+            pupils = new Pupil[] { pupil1, pupil2, pupil3, pupil4 };
+        }
+
+        public static void Show()
+        {
+           
         }
     }
     class Program
     {
         static void Main(string[] args)
         {
-         //   Pupil pupil = new Pupil("1","2",3,4);
-        
+            Pupil pupil1 = new Pupil("Алексей");
+            Pupil pupil2 = new BadPupil("Владимир");
+            Pupil pupil3 = new GoodPupil("Александр");
+            Pupil pupil4 = new ExcelentPupil("Григорий");
+            var ClassRomm = new ClassRoom(pupil1, pupil2, pupil3, pupil4);
         }
     }
 }
